9b3109f68e433422da3d2064194dcb431d2b7e05
Sudheer121 <sudheertripathithebest@gmail.com>
2020-04-23 16:07:53 +0530

    Adding Heroku requirement files

--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-import sqlite3
+#import sqlite3
 from db import db
 
 class UserModel(db.Model):
--- resources/user.py
+++ resources/user.py
@@ -1,4 +1,4 @@
-import sqlite3
+#import sqlite3
 from flask_restful import Resource,reqparse
 from models.user import UserModel
 class UserRegister(Resource):
